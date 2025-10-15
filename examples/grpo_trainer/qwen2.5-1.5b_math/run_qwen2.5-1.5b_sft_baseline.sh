c22d90f4304da3660b58ed79633a3d8a159c8ef5
supyuxiang <asd19780427@mail.dlut.edu.cn>
2025-10-15 10:40:42 +0000

    initial

--- examples/grpo_trainer/qwen2.5-1.5b_math/run_qwen2.5-1.5b_sft_baseline.sh
+++ examples/grpo_trainer/qwen2.5-1.5b_math/run_qwen2.5-1.5b_sft_baseline.sh
@@ -2,20 +2,20 @@ set -x
 
 #export HF_ENDPOINT=https://hf-mirror.com
 
-export CUDA_VISIBLE_DEVICES=1,2,3,8
+export CUDA_VISIBLE_DEVICES=6,7,8,9
 n_gpus_per_node=4
 
-model_path=/data1/yxfeng/models/qwen/Qwen2.5-0.5B-Instruct
-train_file=/data1/yxfeng/data/gsm8k/train.parquet
-val_file=/data1/yxfeng/data/gsm8k/test.parquet
-save_root=/data1/yxfeng/grpo/baseline/qwen/Qwen2.5-0.5B-Instruct/gsm8k/gsm8k_qwen25_0d5b_sft_baseline_$(date +%Y%m%d_%H%M%S)
+model_path=/data1/yxfeng/models/qwen/Qwen2.5-1.5B-Instruct
+train_file=/data1/yxfeng/data/math/train.parquet
+val_file=/data1/yxfeng/data/math/test.parquet
+save_root=/data1/yxfeng/grpo/baseline/qwen/Qwen2.5-1.5B-Instruct/math/math_qwen25_1d5b_sft_baseline_$(date +%Y%m%d_%H%M%S)
 train_batch_size=32
 micro_batch_size_per_gpu=4
 learning_rate=1e-4
 total_epochs=10
 save_freq=100
 test_freq=25
-run_name=qwen2.5_0.5b_sft_baseline_gsm8k_ep${total_epochs}_lr${learning_rate}
+run_name=qwen2.5_1.5b_sft_baseline_math_ep${total_epochs}_lr${learning_rate}
 
 python3 -m verl.trainer.fsdp_sft_trainer \
     data.train_files="$train_file" \
@@ -33,7 +33,7 @@ python3 -m verl.trainer.fsdp_sft_trainer \
     model.use_liger=False \
     model.enable_gradient_checkpointing=True \
     model.gradient_checkpointing_kwargs.max_chunk_size=8192 \
-    trainer.project_name=verl_baseline_sft_qwen2.5_0.5b_gsm8k \
+    trainer.project_name=verl_baseline_sft_qwen2.5_1.5b_math \
     trainer.experiment_name=$run_name \
     trainer.logger='["console","swanlab"]' \
     trainer.n_gpus_per_node=$n_gpus_per_node \
